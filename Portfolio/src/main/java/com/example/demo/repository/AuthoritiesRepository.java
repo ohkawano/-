7b90bdea0774b9b37caf02d96fc7f535690346f1
ohkawano <shiho.flcl06@gmail.com>
2020-12-03 21:53:21 +0900

    [add]コメント文の追加

--- Portfolio/src/main/java/com/example/demo/repository/AuthoritiesRepository.java
+++ Portfolio/src/main/java/com/example/demo/repository/AuthoritiesRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Authorities;
 
+//管理者アカウントのRepository
 public interface AuthoritiesRepository extends JpaRepository<Authorities, String> {
 
 }
--- Portfolio/src/main/java/com/example/demo/repository/TalkRepository.java
+++ Portfolio/src/main/java/com/example/demo/repository/TalkRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Talk;
 
+//コメント投稿のためのRepository
 public interface TalkRepository extends JpaRepository<Talk, Long> {
 
 }
